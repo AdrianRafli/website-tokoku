83009c2dc1d9871b008b6d0c9d4b96d8dd6e58f9
AdrianRafli <adrianrafly20@gmail.com>
2021-07-29 18:32:48 +0700

    Changing header with bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,9 @@
-/*===== MENU SHOW (Grid)=====*/
-const showMenu = () => {
-  const toggle = document.getElementById("nav-toggle");
-  const nav = document.getElementById("nav-menu");
-
-  if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show");
-    });
-  }
-};
-
-showMenu();
-
-/*===== REMOVE MENU =====*/
-const navLink = document.querySelectorAll(".nav_link");
-const navMenu = document.getElementById("nav-menu");
-
-function linkAction() {
-  navMenu.classList.remove("show");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-/*===== SCROLL SECTIONS ACTIVE LINK =====*/
-
 /*===== CHANGE COLOR HEADER =====*/
-
 window.onscroll = () => {
-  const nav = document.getElementById("header");
+  const nav = document.getElementById("navbar");
   if (this.scrollY >= 450) {
-    nav.classList.add("scroll-header");
+    nav.classList.add("scroll-navbar");
   } else {
-    nav.classList.remove("scroll-header");
+    nav.classList.remove("scroll-navbar");
   }
 };
