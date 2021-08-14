7bbe363662e29e1e458e890d4a80798bc473d610
AdrianRafli <adrianrafly20@gmail.com>
2021-08-14 17:24:09 +0700

    Rename Mainjs to homeHeaderjs

--- index.php
+++ index.php
@@ -300,7 +300,7 @@
       <p class="footer_copy">&#169; 2021 Kelompok 1. XII RPL 1</p>
     </footer>
     <!--===== JavaScript =====-->
-    <script src="assets/js/main.js"></script>
+    <script src="assets/js/homeHeader.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
