31ac76fdf6ae8fc0d9dbabf91fce7e3151406930
AdrianRafli <adrianrafly20@gmail.com>
2021-08-17 21:20:59 +0700

    Changes Display Product Title Page into Product Name

--- display-product.php
+++ display-product.php
@@ -95,7 +95,10 @@
     <!-- ===== Bootstrap ===== -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
 
-    <title>Tokoku | Display Product</title>
+    <?php 
+      $produk = mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM produk WHERE idkategori='$idk' AND idproduk='$idp'"));
+    ?>
+    <title>Tokoku | <?= $produk["namaproduk"] ?></title>
   </head>
   <body>
     <!--===== HEADER =====-->
