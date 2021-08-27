58baeeb245ff04a30184c9e678247f6ab3cf19e0
AdrianRafli <adrianrafly20@gmail.com>
2021-08-27 07:42:05 +0700

    Changes Database connection to remote

--- dbconnect.php
+++ dbconnect.php
@@ -3,10 +3,10 @@
 error_reporting(0);
 
 // isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("localhost","root","","tokoku");
+// $conn = mysqli_connect("localhost","root","","tokoku");
 
 // Remote Database
-// $conn = mysqli_connect("remotemysql.com","JCRhGoPFSc","kHLKkD75Sp","JCRhGoPFSc");
+$conn = mysqli_connect("remotemysql.com","JCRhGoPFSc","kHLKkD75Sp","JCRhGoPFSc");
 
 if(!$conn){
 	echo "Gagal Koneksi Database";
